7786022244c6c3be3a2c541885ce1e29a9a65973
Brandon Marrero <brandonmar97@gmail.com>
2020-04-06 22:11:44 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,19 @@
 class Course 
   
+  @@all = []
+  
+  def self.reset_all
+    @@all = @@all.clear 
+  end 
+  
   attr_accessor :title, :schedule, :description
+  
+  def initialize(title, schedule, description)
+    @title = title 
+    @schedule = schedule 
+    @description = description
+    @@all << self
+  end 
+  
 end 
 
